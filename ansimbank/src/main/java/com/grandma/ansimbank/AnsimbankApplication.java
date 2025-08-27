6c9bc318c8077a5a3b1e6fbe74c2542ce7d1c786
KIM HUIJU <gmlwnchlrh@gmail.com>
2025-08-27 13:24:07 +0900

    feat: ansimbank Spring Boot 프로젝트 초기 설정 (#1)

---
+++ ansimbank/src/main/java/com/grandma/ansimbank/AnsimbankApplication.java
@@ -0,0 +1,13 @@
+package com.grandma.ansimbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsimbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsimbankApplication.class, args);
+	}
+
+}
---
+++ ansimbank/src/test/java/com/grandma/ansimbank/AnsimbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grandma.ansimbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsimbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
